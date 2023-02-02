3f6a31198b8dcda1f51bc0741e9a85ccba98647d
Kaciade <valerypolyakov60@gmail.com>
2023-02-03 01:57:08 +0400

    Задание 3 выполнено

---
+++ Tasks/Task_03/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int count = 1;
+int res = 1;
+
+while (count <= num)
+{
+    res = res * count;
+    count++;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {num} равно {res}");
