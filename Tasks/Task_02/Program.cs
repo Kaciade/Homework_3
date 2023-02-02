d170083eca571c6ec086b7a689e9dad6cd173e80
Kaciade <valerypolyakov60@gmail.com>
2023-02-03 01:51:05 +0400

    Задание 2 выполнено

---
+++ Tasks/Task_02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Write("Введите число: ");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+int num2 = num1;
+int sum = 0;
+
+while (num2 > 0)
+{
+    int digit = num2 % 10;
+    sum = sum + digit;
+    num2 = num2 / 10;
+}
+
+Console.WriteLine($"Сумма цифр числа {num1} равна {sum}");
