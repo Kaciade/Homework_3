10acd5c143eaa7cafe902110ea085a589ed1b94a
Kaciade <valerypolyakov60@gmail.com>
2023-02-03 01:42:31 +0400

    Задание 1 выполнено

---
+++ Tasks/Task_01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int count = 1;
+int res = 0;
+Console.WriteLine("\nЧисло \t Куб числа");
+
+while (count <= N)
+{
+    res = count * count * count;
+    Console.WriteLine($"{count} \t {res}");
+    count++;
+}
